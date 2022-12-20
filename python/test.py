96dba18eb5d1f005eb2b746c318a785c48a24d54
zack242 <zakaria_009@hotmail.fr>
2022-12-20 13:39:11 +0100

    v1

--- python/test.py
+++ python/test.py
@@ -1,6 +1,5 @@
 import socketio
 import json 
-from kafka import KafkaProducer
 from kafka import KafkaConsumer
 
 sio = socketio.Client()
@@ -13,12 +12,11 @@ def connect():
 def custom_event(data):
     print(f'Received data: {data}')
 
-sio.connect('http://localhost:3000')
-sio.emit('custom_event', {'data': 'Start connexion'})
+sio.connect('http://127.0.0.1:3000')
 
 consumer = KafkaConsumer()
 consumer.subscribe(['rawTwitter'])
 
 for msg in consumer:
     res = json.loads(msg.value.decode('utf-8')) 
-    sio.emit('custom_event', {'data': res['text']})
+    sio.emit('connection', {'data': res['text']})
