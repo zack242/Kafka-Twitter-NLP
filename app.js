35f928f265dea5ff8552e639fe01215ed4dfd0a7
zack242 <zakaria_009@hotmail.fr>
2022-12-20 15:37:36 +0100

    v3 qui marche

--- app.js
+++ app.js
@@ -1,31 +1,37 @@
-const express = require('express');
-const app = express();
-const http = require('http');
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
+const ejs = require('ejs');
+var kafka = require('kafka-node')
+var express     = require('express');
+var app         = require('express')();
+var server      = require('http').createServer(app);
+var io          = require('socket.io')(server);
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+
+app.set('view engine', 'ejs');
+app.get('/', function(req, res) {
+    res.render('home');
 });
 
-io.on('connection', (socket) => {
-  console.log('User connected');
-  
+io.on('connection', function(socket) {
+    console.log('a user connected');
+    socket.on('chat message', function(msg){
+        console.log('message: ' + msg);
+      });
 });
 
-io.on('connection', (socket) => {
-    socket.on('chat-message', (msg) => {
-      console.log('message: ' + msg);
-    });
-  });
 
-  io.on('connection', (socket) => {
-    socket.on('chat-message', (msg) => {
-      io.emit('chat-message', msg);
-    });
-  });
+Consumer = kafka.Consumer,
+client = new kafka.KafkaClient("localhost:9092"),
+consumer = new Consumer(client,[{ topic: 'rawTwitter', partition: 0, offset: 0 }],{ fromOffset: false });
 
-server.listen(3000, () => {
-  console.log('listening on *:3000');
+consumer.on('message', function (message){
+        console.log("send")
+        io.emit('message', message.value);});
+        
+consumer.on('error', function (err) {
+        console.log('ERROR: ' + err.toString());});
+
+server.listen(process.env.PORT || 3000, function(){
+    console.log('app running');
 });
+
+
--- python/test.py
+++ python/test.py
@@ -1,22 +1,13 @@
-import socketio
 import json 
+import requests
 from kafka import KafkaConsumer
 
-sio = socketio.Client()
-
-@sio.event
-def connect():
-    print('Connected to the server')
-
-@sio.event
-def custom_event(data):
-    print(f'Received data: {data}')
-
-sio.connect('http://127.0.0.1:3000')
+url = "http://localhost:3000"
+headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
 
 consumer = KafkaConsumer()
 consumer.subscribe(['rawTwitter'])
 
 for msg in consumer:
     res = json.loads(msg.value.decode('utf-8')) 
-    sio.emit('connection', {'data': res['text']})
+    requests.post(url, data=msg.value, headers=headers)
